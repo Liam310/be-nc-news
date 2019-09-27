1a27b367071df8baa3ffb998f28648f20f6425b4
Liam Duncan <liam.duncan310@gmail.com>
2019-09-27 10:07:36 +0100

    fix typo

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
 const dbConfig =
   ENV === 'production'
-    ? { client: 'pg', connection: process.env_DATABASE_URL }
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
     : require('../knexfile');
 
 const connection = knex(dbConfig);
